3b154fe1a925deef137bea7275fb4061e74f6731
obedjoshua <uatuthewatcher73@gmail.com>
2026-04-30 17:45:23 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -132,10 +132,11 @@ $header_class = $is_homepage ? 'main-header__home' : 'main-header__home main-hea
                         <svg class="menu-chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="18" height="18"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
                     </label>
                     <div class="site-menu__dropdown">
-                        <a href="#">Buying guides</a>
+                        <a href="<?= $base ?>/pages/news-subcat-pages/buying-guides.php">Buying guides</a>
+                        <a href="<?= $base ?>/pages/sliding-menu-pages/sell-guide.php">Selling Guide</a>
                         <a href="<?= $base ?>/pages/header-pages/news.php">Motor news</a>
-                        <a href="#">Reviews</a>
-                        <a href="#">Advice</a>
+                        <a href="<?= $base ?>/pages/news-subcat-pages/news-reviews.php">Reviews</a>
+                        <a href="<?= $base ?>/pages/sliding-menu-pages/advice.php">Advice</a>
                     </div>
                 </div>
 
